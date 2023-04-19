9182c66e971fd1e36218e5222bc7cc4a718b4241
minhquangphan <phanmin2000@gmail.com>
2023-04-19 15:01:07 +0700

    useState

---
+++ src/useState.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+
+function App() {
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState( () => {
+    const storageJobs = JSON.parse(localStorage.getItem('jobs'))
+    return storageJobs      
+
+  })
+  const handleSubmit = () => {
+    setJobs(prev => {
+      const newJobs = [...prev, job]
+      const jsonJobs = JSON.stringify(newJobs)
+      localStorage.setItem('jobs', jsonJobs)
+
+      return newJobs
+    })
+    setJob('')
+  }
+
+  return (
+    <div style={{padding: 32}}>
+      <input
+        value={job}
+        onChange= {e => setJob(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>{job}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
